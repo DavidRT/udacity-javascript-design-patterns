6b9ae32953c25c47e06bb2150615e49202ab245f
David Rodriguez <drodtach@gmail.com>
2016-05-20 13:42:18 -0400

    Complete Cat Clicker Requirements 2

--- cat-clicker/js/main.js
+++ cat-clicker/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 	var counter = 0;
+	var counter2 = 0;
+
 
 	$('#cat-img').click(function(){
 		counter++;
 		$('.counter').text(counter);
+	});
+
+	$('#cat2-img').click(function(){
+		counter2++;
+		$('.counter2').text(counter2);
 	});
 
 
