29abafda8e5551ab922482566b6f2512ccc27441
David Rodriguez <drodtach@gmail.com>
2016-06-14 13:06:02 -0400

    Complete Cat Clicker Premium Pro

--- cat-clicker-premium-refactored/js/main.js
+++ cat-clicker-premium-refactored/js/main.js
@@ -1,5 +1,7 @@
 $(function() {
   var model = {
+    currentCat: null,
+
     init: function() {
       this.loadCats();
     },
@@ -31,6 +33,28 @@ $(function() {
         }
       ]
     },
+    save: function(cat){
+      
+      var exists = false;
+      var indexAt;
+      this.cats.forEach(function(item,index){
+        if ( cat.catName == item.catName){
+          exists = true;
+          indexAt = index;
+        }
+      });
+
+      if ( !exists ){
+        cat.catId = this.cats.length+1;
+        this.cats.push(cat);
+      }else{
+        this.cats[indexAt].catName = cat.catName;
+        this.cats[indexAt].counter = cat.counter;
+        this.cats[indexAt].catImage = cat.catImage;
+      }
+      
+
+    },
     getAllCats: function() {
       return this.cats;
     },
@@ -51,11 +75,15 @@ $(function() {
       });
     },
   };
+
+/* ======= Octopus ======= */
+
   var octopus = {
     init: function() {
       model.init();
       viewList.init();
       viewCat.init();
+      adminView.init();
     },
     getCats: function() {
       return model.getAllCats();
@@ -66,13 +94,34 @@ $(function() {
     getCatById: function(id) {
       return model.getCatById(id);
     },
+    displayAdminArea: function(){
+      adminView.render();
+    },
+    disableAdminArea: function(){
+      adminView.disable();
+    },
+    saveCat: function(cat){
+      model.save(cat);
+      viewList.render();
+      adminView.disable();
+    },
+    setCurrentCat: function(cat){
+      model.currentCat = cat;
+    },
+    getCurrentCat: function(cat){
+      return model.currentCat;
+    },
   };
+
+/* ======= Cat List View ======= */
+
   var viewList = {
     init: function() {
       this.catList = $('#cats');
       this.catList.on('click', '.cat', function(e) {
         var x = $(this).data('id')
         viewCat.render(x);
+         adminView.render();
       });
       viewList.render();
     },
@@ -85,6 +134,9 @@ $(function() {
       this.catList.html(htmlStr);
     },
   };
+
+/* ======= Cat Detail View ======= */ 
+
   var viewCat = {
     init: function() {
       this.nameCat = $('#cat-name');
@@ -93,6 +145,7 @@ $(function() {
     },
     render: function(idCat) {
       var cat = octopus.getCatById(idCat);
+      octopus.setCurrentCat(cat);
       viewCat.nameCat.html(cat.catName)
       viewCat.renderCounter(cat);
       viewCat.imageCat.attr('src', cat.catImage)
@@ -106,5 +159,62 @@ $(function() {
       viewCat.counterCat.html("Clicks counter: " + cat.counter);
     }
   };
+
+/* ======= Admin View ======= */
+
+
+  var adminView = {
+    init: function(){
+      this.adminButton = document.getElementById('adminButton');
+      this.adminArea = $('#admin-area');
+      this.cancelAdminAreaBtn = document.getElementById('cancel-admin');
+      this.adminSaveBtn = document.getElementById('admin-save');
+
+      this.inptName = $('#frm-name');
+      this.inptImage = $('#frm-image');
+      this.inptCounter = $('#frm-counter');
+
+      adminView.bind();
+    },
+
+    bind: function(){
+      adminView.adminButton.addEventListener('click', function(){
+        octopus.displayAdminArea();
+      });
+
+      adminView.cancelAdminAreaBtn.addEventListener('click',function(){
+        octopus.disableAdminArea();
+      });
+
+      adminView.adminSaveBtn.addEventListener('click',function(){
+        
+        var obj = {};
+        obj.catName = adminView.inptName.val();
+        obj.catImage = adminView.inptImage.val();
+        obj.counter = adminView.inptCounter.val();
+        octopus.saveCat(obj);
+      });
+    },
+    clear: function(){
+      adminView.inptCounter.val('');
+      adminView.inptImage.val('');
+      adminView.inptName.val('');
+    },
+
+    render: function(){
+      adminView.clear();
+      if ( octopus.getCurrentCat() ){
+         adminView.inptName.val(octopus.getCurrentCat().catName);
+         adminView.inptCounter.val(octopus.getCurrentCat().counter)
+         adminView.inptImage.val(octopus.getCurrentCat().catImage)
+      }
+      adminView.adminArea.show();
+    },
+
+    disable: function(){
+      adminView.adminArea.hide();
+    }
+  };
+
   octopus.init();
 });
