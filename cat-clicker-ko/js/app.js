5711105ef080dc88241043d591e47f8d5daa6be1
David Rodriguez <drodtach@gmail.com>
2016-06-28 18:13:35 -0400

    Implement control flow to display nicknames

--- cat-clicker-ko/js/app.js
+++ cat-clicker-ko/js/app.js
@@ -2,7 +2,7 @@ var ViewModel = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
-  this.imgAttribution = ko.observable('https://www.google.com');}
+  this.imgAttribution = ko.observable('https://www.google.com');
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
@@ -18,5 +18,14 @@ var ViewModel = function() {
       return "Super";
     }
   }, this);
+
+  this.nicknames = ko.observableArray([
+    { name: "Bungle" },
+    { name: "George" },
+    { name: "Zippy" }
+	]);
+
+  console.log(this.nicknames());
+
 }
 ko.applyBindings(new ViewModel());
