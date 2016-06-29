7ab0b18c3a9e820f5a3ec9e62541b5bd8d9b3265
David Rodriguez <drodtach@gmail.com>
2016-06-29 18:01:51 -0400

    Move model logic to cat function

--- cat-clicker-ko/js/app.js
+++ cat-clicker-ko/js/app.js
@@ -1,13 +1,10 @@
-var ViewModel = function() {
+var Cat = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
   this.imgAttribution = ko.observable('https://www.google.com');
 
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
-  
+
   this.levelCat = ko.computed(function() {
     var count = this.clickCount();
     if (count < 10) {
@@ -23,9 +20,15 @@ var ViewModel = function() {
     { name: "Bungle" },
     { name: "George" },
     { name: "Zippy" }
-	]);
+  ]);
 
-  console.log(this.nicknames());
+};
 
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
 }
+
 ko.applyBindings(new ViewModel());
